e367911d60516b406313bffc4e8942c4602ed4e5
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-02-20 20:01:04 +0100

    ✨feat(TweeForm): ajout de l'id de l'utilisateur à la création d'un tweet

--- client/src/components/Common/Tweet.js
+++ client/src/components/Common/Tweet.js
@@ -11,7 +11,6 @@ const Tweet = ({ tweet }) => {
     <div className="post">
       <p>{tweet.body}</p>
       <p>{tweet.user_id}</p>
-      <p>{user.first_name}</p>
 
       <div className="author">
         <Like tweet={tweet} />
