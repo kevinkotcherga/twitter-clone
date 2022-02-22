8ee3cac2479fc13f1584de663b04f6d547b5368d
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-02-22 17:53:30 +0100

    changes

--- client/src/apis/authentication.js
+++ client/src/apis/authentication.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const baseUrl = 'http://localhost:3001/';
-const baseUrl = 'https://re-testing-heroku-yeah.herokuapp.com/';
+const baseUrl = 'https://twitter-clone-kotcherga.herokuapp.com/';
 
 const login = payload => axios.post(`${baseUrl}api/v1/login`, payload);
 
